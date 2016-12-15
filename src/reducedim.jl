3cf9043e0d2aec2f5e2b6c1af53100ed4078ff1c
Hiroyuki Shindo <hshindo@outlook.com>
2016-12-15 16:36:05 +0900

    Add reducedim_kernel

--- src/reducedim.jl
+++ src/reducedim.jl
@@ -1,8 +1,7 @@
 import Base: sum, max
-export argmax
+export reducedim_kernel, argmax
 
 @generated function sum{T,N}(x::CuArray{T,N}, dim::Int)
-    y = similar(x, ntuple(i -> i==dim ? 1 : size(x,i), N))
     t = ctype(T)
     f = compile("""
     $array_h
@@ -18,6 +17,7 @@ export argmax
         if (threadIdx.y == 0) y(blockIdx.x, 0, blockIdx.z) = val;
     } """)
     quote
+        y = similar(x, ntuple(i -> i==dim ? 1 : size(x,i), N))
         x = reshape3d(x, dim)
         $f(size(x,1), size(x,2), size(x,3), x, reshape3d(y,dim), bx=1, by=128, sharedmem=sizeof(T)*32)
         y
@@ -41,46 +41,54 @@ function reshape3d(x::CuArray, dim::Int)
 end
 
 function reducedim_kernel(op::String, v0::String)
-    f = compile("""
-    $array_h
+    @generated function reducedim{T,N}(x::CuArray{T,N}, dim::Int)
+        t = ctype(T)
+        f = compile("""
+        $array_h
 
-    template<typename T>
-    __inline__ __device__ T warpReduce(T v0) {
-        for (int offset = warpSize/2; offset > 0; offset /= 2) {
-            T v1 = __shfl_down(v0, offset);
-            v0 = $op;
+        template<typename T>
+        __inline__ __device__ T warpReduce(T v0) {
+            for (int offset = warpSize/2; offset > 0; offset /= 2) {
+                T v1 = __shfl_down(v0, offset);
+                v0 = $op;
+            }
+            return v0;
         }
-        return v0;
-    }
 
-    template<typename T>
-    __inline__ __device__ T blockReduce(T val) {
-        static __shared__ T shared[32];
-        int lane = threadIdx.y % warpSize;
-        int wid = threadIdx.y / warpSize;
+        template<typename T>
+        __inline__ __device__ T blockReduce(T val) {
+            static __shared__ T shared[32];
+            int lane = threadIdx.y % warpSize;
+            int wid = threadIdx.y / warpSize;
 
-        val = warpReduce<T>(val);
-        if (lane == 0) shared[wid] = val;
-        __syncthreads();
+            val = warpReduce<T>(val);
+            if (lane == 0) shared[wid] = val;
+            __syncthreads();
 
-        val = (lane < 4) ? shared[lane] : 0;
-        //if (wid == 0) val = warpReduce<T>(val);
-        val = warpReduce<T>(val);
-        return val;
-    }
+            val = (lane < 4) ? shared[lane] : 0;
+            //if (wid == 0) val = warpReduce<T>(val);
+            val = warpReduce<T>(val);
+            return val;
+        }
 
-    __global__ void f(Array<$t,3> x, Array<$t,$N> y) {
-        int idx_x = blockIdx.x * blockDim.x + threadIdx.x;
-        int idx_y = blockIdx.y * blockDim.y + threadIdx.y;
-        int idx_z = blockIdx.z * blockDim.z + threadIdx.z;
-        if (idx_y >= x.dims[1]) return;
+        __global__ void f(Array<$t,3> x, Array<$t,3> y) {
+            int idx_x = blockIdx.x * blockDim.x + threadIdx.x;
+            int idx_y = blockIdx.y * blockDim.y + threadIdx.y;
+            int idx_z = blockIdx.z * blockDim.z + threadIdx.z;
+            if (idx_y >= x.dims[1]) return;
 
-        $t val = x(idx_x, idx_y, idx_z);
-        val = blockReduce<$t>(val);
-        if (threadIdx.y == 0) y(blockIdx.x, 0, blockIdx.z) = val;
-    } """)
-    quote
+            $t val = x(idx_x, idx_y, idx_z);
+            val = blockReduce<$t>(val);
+            if (threadIdx.y == 0) y(blockIdx.x, 0, blockIdx.z) = val;
+        } """)
+        quote
+            y = similar(x, ntuple(i -> i==dim ? 1 : size(x,i), N))
+            x = reshape3d(x, dim)
+            $f(size(x,1), size(x,2), size(x,3), x, reshape3d(y,dim), bx=1, by=128, sharedmem=sizeof(T)*32)
+            y
+        end
     end
+    reducedim
 end
 
 const reducedim_h = """
