769b99d0cdd54c7342b01cc437bac6369509f6f0
Hiroyuki Shindo <hshindo@outlook.com>
2016-12-18 10:52:44 +0900

    Update functions

--- src/cudnn/CUDNN.jl
+++ src/cudnn/CUDNN.jl
@@ -46,7 +46,7 @@ include("convolution.jl")
 #include("dropout.jl")
 include("filter.jl")
 ##include("lrn.jl")
-#include("pooling.jl")
+include("pooling.jl")
 include("softmax.jl")
 ##include("rnn.jl")
 
--- src/function.jl
+++ src/function.jl
@@ -27,6 +27,42 @@ end
 
 box(x) = pointer_from_objref(x)
 
+@generated function typesym{T}(::Type{T})
+    syms = (Symbol(T),)
+    :($syms[1])
+end
+@generated function typesym{T1,T2}(::Type{T1}, ::Type{T2})
+    syms = (Symbol(T1, "_", T2),)
+    :($syms[1])
+end
+
+macro compile(key, code)
+    
+
+    src.head == :string || throw("expr is not string")
+    dict = Dict()
+    syms = Any[string(gensym())]
+    for arg in src.args
+        typeof(arg) != Symbol && continue
+        haskey(dict, arg) && continue
+        dict[arg] = arg
+        push!(syms, "_", arg)
+    end
+    expr = Expr(:string, syms...)
+    quote
+        local key = $(esc(expr))
+        if haskey(cufuns, key)
+            cufuns[key]
+        else
+            local code = $(esc(src))
+            local ptx = NVRTC.compile(code)
+            f = load_ptx(ptx)
+            cufuns[key] = f
+            f
+        end
+    end
+end
+
 function (f::CuFunction)(dx::Int, dy::Int, dz::Int, args...;
     bx=128, by=1, bz=1, sharedmem=0, stream=C_NULL)
 
